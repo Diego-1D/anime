084159fb1d86b326228247de6929ac23a9994afa
Marcos Amorim <markstm10@gmail.com>
2021-11-08 01:25:48 -0300

    🛠 refactor: updated  context to load title from fetch response. Updated some miscellaneous components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import FeatureAnime from './components/FeatureAnime';
-import Hero from './components/Hero';
-import { GlobalStyle } from './globalStyles';
+import React from "react";
+import FeatureAnime from "./components/FeatureAnime";
+import Hero from "./components/Hero";
+import { GlobalStyle } from "./globalStyles";
 
 function App() {
-
   return (
     <>
       <GlobalStyle />
       <Hero />
-      <FeatureAnime/>
+      <FeatureAnime />
+      <div>Hello world</div>
     </>
   );
 }
 
 export default App;
-
-
--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-    width: 100%;
-    height: 100vh;
-    background-color: #050505;
+  width: 100%;
+  height: 100vh;
+  background-color: #050505;
 `;
