13bed7836aa0eb1ff3bd4320432e77e615babbde
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-11-09 00:29:56 -0300

    Alterações simples

--- src/context/index.js
+++ src/context/index.js
@@ -1,51 +1,59 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from "react";
 
-const url = 'https://api.aniapi.com/v1/anime?title=';
+const url = "https://api.aniapi.com/v1/anime?title=";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [animes, setAnimes] = useState([]);
+  const [serchAnime, setSearchAnime] = useState('a');
 
-    const [animes, setAnimes] = useState([]);
-    const [titleAnime, setTitleAnime] = useState([]);
-    const [movieList, setMovieList] = useState([]);
+  const fetchAnimes = useCallback(async () => {
+    try {
+      const response = await fetch(`${url}${serchAnime}`);
+      const dataAnime = await response.json();
+      const { data } = dataAnime;
+      const { documents } = data;
 
-    const fetchAnimes = useCallback(async () => {
-        try {
-            const response = await fetch(`${url}`);
-            const dataAnime = await response.json()
-            const { data } = dataAnime;
-            const { documents } = data;
+      if (documents) {
+        const newAnimes = documents.map((item) => {
+          //pra funcionar como tu precisava, faltava tu puxar o 'titles' de dentro do item tbm
 
-            if (documents) {
-                const newAnimes = documents.map((item) => {
-                    const { anilist_id, cover_image } = item;
-                    return {
-                        id: anilist_id,
-                        image: cover_image,
-                    }
-                })
-                setAnimes(newAnimes);
-            }
+          //const { anilist_id, cover_image } = item;
+          const { anilist_id, cover_image, titles, genres } = item;
+          return {
+            id: anilist_id,
+            image: cover_image,
+            //então inseria aqui
+            titles: titles,
+            genres: genres,
+          };
+        });
+        setAnimes(newAnimes);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [serchAnime]);
 
-        } catch (error) {
-            console.log(error);
-        }
-    }, [])
+  useEffect(() => {
+    fetchAnimes();
+  }, [serchAnime,fetchAnimes]);
 
-    useEffect(() => {
-        fetchAnimes();
-    }, [fetchAnimes])
-
-
-    return <AppContext.Provider
-        value={{
-            animes,
-        }}>{children}</AppContext.Provider>
-}
+  return (
+    <AppContext.Provider
+      value={{
+        animes,
+        setSearchAnime
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 
 export const useGlobalContext = () => {
-    return useContext(AppContext);
-}
+  return useContext(AppContext);
+};
 
 export { AppContext, AppProvider };
