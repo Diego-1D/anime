6a881e69d34642a84c39e7dd11d57c69bdbdb70c
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-11-09 00:29:04 -0300

    criação do componente SearchAnimes

---
+++ src/components/SearchAnimes/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+    SearchArea,
+    SearchForm,
+    SearchTitle,
+    SearchFormControl,
+    Input
+} from './styles';
+import { useGlobalContext } from '../../context/index';
+
+const SearchAnime = () => {
+    const { setSearchAnime } = useGlobalContext();
+    const searchValue = React.useRef('');
+
+    React.useEffect(() => {
+        searchValue.current.focus();
+    }, [])
+
+    const search = () => {
+        setSearchAnime(searchValue.current.value);
+    }
+
+    const handleSubmit = (s) => {
+        s.preventDefault();
+    }
+    return (
+        <SearchArea>
+            <SearchForm onSubmit={handleSubmit}>
+                <SearchFormControl>
+                    <SearchTitle>Buscar:</SearchTitle>
+                    <Input
+                        type='text'
+                        id='name'
+                        ref={searchValue}
+                        onChange={search}
+                    />
+                </SearchFormControl>
+            </SearchForm>
+        </SearchArea>
+    )
+
+}
+
+export default SearchAnime;
---
+++ src/components/SearchAnimes/styles.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const SearchArea = styled.section`
+    padding: 2rem;
+`;
+
+export const SearchForm =styled.form`
+    width: 40rem;
+    margin: 0 auto;
+    padding: 2rem 2.5rem;
+    text-transform: capitalize;
+    border-radius: 0.25rem;
+    border-color: pink;
+
+`;
+
+export const SearchFormControl = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const SearchTitle = styled.h2`
+        font-size: 1.1rem;
+        color: #FFFFFF;
+        margin-right: 0.5rem;
+`;
+
+export const Input = styled.input`
+    width: 100%;
+    border: 1px;
+    color: #FFFFFF;
+    background: #363636;
+    border-radius: 0.25rem;
+    padding: 0.5rem;
+    font-size: 1.1rem;
+`;
