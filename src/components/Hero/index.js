0c6d556e92ab4026190fc44107096438c990d6b8
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-11-11 00:18:23 -0300

    Criação do componente Hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,14 +1,80 @@
-import React from 'react';
-
-import { 
-    Container
+import React, { useEffect, useState, useCallback } from 'react';
+import {
+    Container,
+    Title,
+    FeaturedVertical,
+    FeaturedHorizontal,
+    Description,
+    Info,
+    Year,
+    Seasons,
+    Points
 } from './styles';
 
+const url = "https://api.aniapi.com/v1/anime?title=One%20Piece&formats=0,1&status=1&year=1999&season=3&genres=Pirates,War,Cyborg&nsfw=true";
+
 const Hero = () => {
+
+    const [anime, setAnime] = useState([]);
+
+    const fetchAnimes = useCallback(async () => {
+        try {
+            const response = await fetch(`${url}`);
+            const dataAnime = await response.json();
+            const { data } = dataAnime;
+            const { documents } = data
+
+            if (documents) {
+                const newAnime = documents.map((item) => {
+                    const { anilist_id, banner_image, titles, cover_image,score, descriptions, season_period, season_year} = item;
+                    console.log('Banner: ', banner_image)
+                    console.log('Image: ', cover_image)
+                    return {
+                        id: anilist_id,
+                        titles: titles,
+                        image: banner_image,
+                        cover: cover_image,
+                        score:score,
+                        descriptions: descriptions,
+                        seasons: season_period,
+                        year: season_year,
+                    }
+                })
+                setAnime(newAnime);
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchAnimes();
+    }, [fetchAnimes]);
+
     return (
-    <Container>
-    </Container>
-)
+        <>
+            {
+                anime.map((item) => {
+                    return (
+                        <Container style={{ backgroundImage: `url(${item.image})` }}>
+                            <FeaturedVertical>
+                                <FeaturedHorizontal>
+                                    <Title>{item.titles.en}</Title>
+                                    <Description>{item.descriptions.it}</Description>
+                                    <Info>
+                                        <Points>{item.score} Pontos</Points>
+                                        <Year>{item.year}</Year>
+                                        <Seasons>{item.seasons} Temporadas</Seasons>
+                                    </Info>
+                                </FeaturedHorizontal>
+                            </FeaturedVertical>
+                        </Container>
+                    )
+                })
+            }
+        </>
+    )
 }
 
 export default Hero;
--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,7 +1,52 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  width: 100%;
-  height: 100vh;
-  background-color: #050505;
+  height: 68vh;
+  background-repeat: no-repeat;
+`;
+
+export const FeaturedVertical = styled.div`
+  width: inherit;
+  height:inherit;
+  background:  linear-gradient(to top, #050303 10%, transparent 90%);
+`;
+
+export const FeaturedHorizontal = styled.div`
+  width: inherit;
+  height: inherit;
+  background: linear-gradient(to right,  #050303 30%, transparent 70%);
+`;
+
+export const Title = styled.h1`
+  font-size: 60px;
+  font-weight: bold;
+  color: #FFFFFF;
+`;
+
+export const Description = styled.h2`
+  font-size: 20px;
+  color:#999;
+  max-width: 40%;
+`;
+
+export const Info = styled.div`
+  width: 30%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+export const Year = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+  color: #FFFF;
+`;
+
+export const Points = styled.h3`
+  color: #46d369;
+`;
+
+export const Seasons = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+  color: #FFFF
 `;
