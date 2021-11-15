407be3cd5cb0c42d8ee2edb027cf4af1c97a0693
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-11-15 01:02:23 -0300

    Criação da tela Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FeatureAnime from '../../components/FeatureAnime';
+import Hero from '../../components/Hero';
+
+const Home = () => {
+    return (
+        <main>
+            <Hero />
+            <FeatureAnime />
+        </main>
+    )
+}
+
+export default Home;
