397839f2a587f2559c2b32799a6196b36b79aa63
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-11-16 23:33:56 -0300

    criação do componente filtragem

--- src/components/FeatureAnime/index.js
+++ src/components/FeatureAnime/index.js
@@ -2,22 +2,67 @@ import React, { useState, useEffect } from 'react';
 import AnimeList from '../AnimeList';
 import MenuCategories from '../MenuCategories';
 import { useGlobalContext } from '../../context';
-import { useLocation } from 'react-router';
+import Loading from "../Loading";
 import {
     Container
 } from './styles';
+import { useLocation } from 'react-router';
 
-const allCategories = ['Todos', 'Drama', 'Fantasy', 'Mahou Shoujo', 'Mystery', 'Psychological', 'Thriller', 'Alternate Universe', 'Anti-Hero']
+const allCategories = ['Todos', 'Comedy', 'Action', 'Mahou Shoujo', 'Mystery', 'Psychological', 'Thriller', 'Alternate Universe', 'Anti-Hero']
 
-
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 const FeatureAnime = () => {
-
+    const {loading} = useGlobalContext();
+    let query = useQuery();
     const { animes } = useGlobalContext();
     const [categories, setCategories] = useState(allCategories);
+    const [menufixed, setMenuFixed] = useState(false);
+    const [menuItems, setMenuItems] = useState([]);
+
+    useEffect(() => {
+        setMenuItems(animes);        
+    }, [animes])
+    
+    useEffect(() => {
+        const scrollListener = () => {
+            if (window.scrollY > 450) {
+                setMenuFixed(true);
+            } else {
+                setMenuFixed(false);
+            }
+        }
+        window.addEventListener('scroll', scrollListener);
+        return () => {
+            window.removeEventListener('scroll', scrollListener);
+        }
+    }, [])
+
+    const filterAnime = (a) => {
+        if (a === 'Todos') {
+            setMenuItems(animes);
+
+        }
+        const newAnime = animes.filter((item)=> item.genres[2] === a);
+        setMenuItems(newAnime);
+    }
+
+    useEffect(()=>{
+        let a = query.get('genres');
+        if (a != null){
+            filterAnime(a);
+        }
+    },[]);
+
+    if (loading) {
+        return <Loading/>
+      }
+
     return (
         <Container>
-            <MenuCategories categories={categories} />
-            <AnimeList animes={animes}/>
+            <MenuCategories filterAnime={filterAnime} categories={categories} menuFixed={menufixed} />
+            <AnimeList animes={menuItems} menuFixed={menufixed} />
         </Container>
     )
 }
