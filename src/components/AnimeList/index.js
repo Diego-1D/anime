e47bb98590174bf70443d063a3211fbb8be14196
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-11-24 22:43:11 -0300

    comando para quando o objeto estiver vazio

--- src/components/AnimeList/index.js
+++ src/components/AnimeList/index.js
@@ -2,22 +2,18 @@ import React from "react";
 import { Container, AnimeArea, Message } from "./styles";
 import SearchAnime from '../SearchAnimes';
 import Anime from "./Anime";
-import { useGlobalContext } from "../../context";
 
 const AnimeList = ({ animes, menuFixed }) => {
 
-  const { loading } = useGlobalContext();
-
-  if (animes.length < 1) {
-    return (
-      <Message>Nenhum anime foi encontrado!</Message>
-    )
-  }
 
   return (
     <Container menuFixed={menuFixed}>
       <SearchAnime />
+      {animes.length < 1 &&
+        <Message>Nenhum anime foi encontrado!</Message>
+      }
       <AnimeArea>
+
         {animes.map((item) => {
           //alterei a forma como tu tá enviando os parametros para o componente filho
           // return <Anime key={item.id} {...item}/>
