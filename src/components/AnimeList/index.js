58b50c89cfd6624c66a2663719fe666d6506c233
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-01-23 16:49:16 -0300

    Filter responsivel

--- src/components/AnimeList/index.js
+++ src/components/AnimeList/index.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { Container, AnimeArea, Message } from "./styles";
+import { Container, AnimeArea, Message, Wrapper, Select, Option, Title } from "./styles";
 import SearchAnime from '../SearchAnimes';
 import Anime from "./Anime";
 import { useGlobalContext } from "../../context";
 import ReactLoading from 'react-loading';
 
-const AnimeList = ({ animes, menuFixed }) => {
+const AnimeList = ({ animes, categories, filterAnime }) => {
 
   const { loading } = useGlobalContext();
 
   return (
-    <Container menuFixed={menuFixed}>
-
+    <Container>
+      <Wrapper>
+        <Title>Categorias</Title>
+        <Select
+          onClick={(e) => filterAnime(e.target.value)}
+        >
+          {
+            categories.map((item, key) =>
+              <Option value={item} key={key}>{item}</Option>
+            )
+          }
+        </Select>
+      </Wrapper>
       <SearchAnime />
       {loading ?
         <ReactLoading type={'spin'} color={'#FFFFFF'} height={'8%'} width={'8%'} />
